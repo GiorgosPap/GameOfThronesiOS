07fbfe708178353bb0b9d4497d0447afbb0deddb
“gipap90” <gipap90@gmail.com>
2018-03-01 23:34:33 +0200

    created Event model

---
+++ GameOfThronesiOS/Event/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  GameOfThronesiOS
+//
+//  Created by Giorgos Papadimitriou on 01/03/2018.
+//  Copyright © 2018 Giorgos Papadimitriou. All rights reserved.
+//
+
+import Foundation
