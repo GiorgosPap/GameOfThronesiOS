0e71834d24082ff238b66a00b0fbc59829be1574
“gipap90” <gipap90@gmail.com>
2018-03-01 23:34:54 +0200

    created Event model

--- GameOfThronesiOS/Event/Event.swift
+++ GameOfThronesiOS/Event/Event.swift
@@ -5,5 +5,26 @@
 //  Created by Giorgos Papadimitriou on 01/03/2018.
 //  Copyright © 2018 Giorgos Papadimitriou. All rights reserved.
 //
+import UIKit
+import ObjectMapper
 
-import Foundation
+class Event: Mappable {
+    //MARK: Properties
+    var name: String?
+    var date: Int?
+    var age: Age?
+    var createdAt: Date?
+    var updatedAt: Date?
+    
+    required init?(map: Map) {
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        name   <- map["name"]
+        date <- map["date"]
+        age <- map["age"]
+        createdAt <- map["createdAt"]
+        updatedAt <- map["updatedAt"]
+    }
+}
